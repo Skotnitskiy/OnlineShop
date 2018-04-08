9c90bb8c77c328a236db856c039cede18658674d
serhSK <sergeyskotnitskiy@gmail.com>
2018-04-08 17:19:49 +0300

    get entities by id

--- base/api_views.py
+++ base/api_views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.utils import json
+
 from base.serializers import CategoriesSerializer, SubCategoriesSerializer, ProductsSerializer, ProducersSerializer
 from rest_framework import viewsets
 from product.models import Product, Subcategory, Category, Producer
@@ -21,3 +26,55 @@ class ProductsViewSet(viewsets.ModelViewSet):
 class ProducersViewSet(viewsets.ModelViewSet):
     queryset = Producer.objects.all()
     serializer_class = ProducersSerializer
+
+
+@csrf_exempt
+def get_producer_by_id(request, pk):
+
+    try:
+        producer = Producer.objects.get(pk=pk)
+    except Producer.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ProducersSerializer(producer)
+        return JsonResponse(serializer.data)
+
+
+@csrf_exempt
+def get_product_by_id(request, pk):
+
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ProductsSerializer(product)
+        return HttpResponse(json.dumps(serializer.data, ensure_ascii=False), content_type="application/json")
+
+
+@csrf_exempt
+def get_category_by_id(request, pk):
+
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CategoriesSerializer(category)
+        return JsonResponse(serializer.data)
+
+
+@csrf_exempt
+def get_subcategory_by_id(request, pk):
+
+    try:
+        subcategory = Subcategory.objects.get(pk=pk)
+    except Subcategory.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SubCategoriesSerializer(subcategory)
+        return JsonResponse(serializer.data)
--- base/urls.py
+++ base/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     url(r'', include('product.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
+    url(r'^api/producer/(?P<pk>[0-9]+)/$', api_views.get_producer_by_id),
+    url(r'^api/product/(?P<pk>[0-9]+)/$', api_views.get_product_by_id),
+    url(r'^api/subcategory/(?P<pk>[0-9]+)/$', api_views.get_subcategory_by_id),
+    url(r'^api/category/(?P<pk>[0-9]+)/$', api_views.get_category_by_id),
 ]
