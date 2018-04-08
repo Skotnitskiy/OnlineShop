fd6551e3395e44a079f2a14e4395127eeb61dba9
serhSK <sergeyskotnitskiy@gmail.com>
2018-04-09 02:02:45 +0300

    fix deploy 2

--- base/urls.py
+++ base/urls.py
@@ -2,20 +2,21 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 
-import api_views
+from base.api_views import CategoriesViewSet, SubCategoriesViewSet, ProductsViewSet, ProducersViewSet, \
+    get_producer_by_id, get_product_by_id, get_subcategory_by_id, get_category_by_id
 
 router = routers.DefaultRouter()
-router.register(r'api/categories', api_views.CategoriesViewSet)
-router.register(r'api/subcategories', api_views.SubCategoriesViewSet)
-router.register(r'api/products', api_views.ProductsViewSet)
-router.register(r'api/producers', api_views.ProducersViewSet)
+router.register(r'api/categories', CategoriesViewSet)
+router.register(r'api/subcategories', SubCategoriesViewSet)
+router.register(r'api/products', ProductsViewSet)
+router.register(r'api/producers', ProducersViewSet)
 
 urlpatterns = [
     url(r'', include('product.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api/producer/(?P<pk>[0-9]+)/$', api_views.get_producer_by_id),
-    url(r'^api/product/(?P<pk>[0-9]+)/$', api_views.get_product_by_id),
-    url(r'^api/subcategory/(?P<pk>[0-9]+)/$', api_views.get_subcategory_by_id),
-    url(r'^api/category/(?P<pk>[0-9]+)/$', api_views.get_category_by_id),
+    url(r'^api/producer/(?P<pk>[0-9]+)/$', get_producer_by_id),
+    url(r'^api/product/(?P<pk>[0-9]+)/$', get_product_by_id),
+    url(r'^api/subcategory/(?P<pk>[0-9]+)/$', get_subcategory_by_id),
+    url(r'^api/category/(?P<pk>[0-9]+)/$', get_category_by_id),
 ]
